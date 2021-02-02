02145331f730bcfc47df0b39233064c42daaac39
Lucas <lucas.werner@viacesi.fr>
2021-02-02 16:20:56 +0100

    Bases route /places

---
+++ api/api/places/read.php
@@ -0,0 +1,16 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: GET");
+    header("Access-Control-Max-Age: 3600");
+
+include_once "../../class/place.php";
+include_once "../../config/database.php";
+
+$database = new Database();
+$db = $database->getConnection();
+
+$place = new Place($db);
+
+?>
