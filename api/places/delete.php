2f9210d9abdd3a83a1a3fbe375e1139f84ed6326
Lucas <lucas.werner@viacesi.fr>
2021-02-03 09:31:58 +0100

    Update arbo + CRUD places

--- api/places/delete.php
+++ api/places/delete.php
@@ -1,19 +1,19 @@
 <?php
     header("Access-Control-Allow-Origin: *");
-    header("Content-Type: charset=UTF-8");
+    header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Methods: POST");
     header("Access-Control-Max-Age: 3600");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     
-    include_once "../../class/place.php";
-    include_once "../../config/database.php";
+    include_once "../class/place.php";
+    include_once "../config/database.php";
     
     $database = new Database();
     $db = $database->getConnection();
     
     $place = new Place($db);
     
-    $placeName = (isset($_GET['nom_emplacement'])) ? $_GET['nom_emplacement'] : '';
+    $placeName = (isset($_GET['nom_emplacement'])) ? ucfirst($_GET['nom_emplacement']) : '';
     
     $place->place_name = $placeName;
     
--- api/places/read.php
+++ api/places/read.php
@@ -1,19 +1,17 @@
 <?php
 
     header("Access-Control-Allow-Origin: *");
-        header("Content-Type: application/json; charset=UTF-8");
-        header("Access-Control-Allow-Methods: GET");
-        header("Access-Control-Max-Age: 3600");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
 
-    include_once "../../class/place.php";
-    include_once "../../config/database.php";
+    include_once "../class/place.php";
+    include_once "../config/database.php";
 
     $database = new Database();
     $db = $database->getConnection();
 
     $place = new Place($db);
-
-    $placeName = (isset($_GET['nom_emplacement'])) ? $_GET['nom_emplacement'] : '';
 
     $stmt = $place->getPlaces();
 
